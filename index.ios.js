2d616def138e301b213f4343275b832f06376282
Benjamin <benjamin.j.arias@gmail.com>
2016-04-21 17:25:47 +0200

    Initial commit

---
+++ index.ios.js
@@ -0,0 +1,22 @@
+import React from 'react';
+var {
+  AppRegistry,
+  MapView,
+  View,
+  StyleSheet
+} = React;
+
+var Weather = React.createClass({
+  render: function() {
+    return <MapView style={styles.map} ></MapView>
+  }
+});
+
+var styles = StyleSheet.create({
+  map: {
+    flex: 1,
+  }
+});
+
+
+AppRegistry.registerComponent('weather', () => Weather);
